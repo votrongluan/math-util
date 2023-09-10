a57e08fb52a238dde694b877bd5dd7de52b35bdf
votrongluan <trongluan115@gmail.com>
2023-09-10 14:27:08 +0700

    refactor the main code and green 4ever always

--- src/com/votrongluan/mathutil/core/MathUtil.java
+++ src/com/votrongluan/mathutil/core/MathUtil.java
@@ -13,21 +13,46 @@ public class MathUtil {
     // giai thừa hàm đồ thị dốc đứng, tăng nhanh về giá trị
     // 20 giai thừa 18 con số 0, vừa kịp đủ cho kiểu long của Java
     // 21 giai thừa tràn kiểu long bài này quy ước n [0..20]
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. N must "
+//                    + "be between 0..20");
+//        }
+//
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        
+//        return product;
+//    }
+    // Học đệ quy trong vòng 30s - Recursion
+    // Hiện tượng gọi lại chính mình với 1 quy mô khác
+    // Ví dụ: con búp bê nga, giống nhau và lồng trong nhau
+    // búp bê to, nhỏ hơn, nhỏ hơn nữa ... đến điểm dừng
+    // n! = 1 . 2 . 3 . 4 . 5 . 6 ... n
+    // 6! = 5! x 6
+    // 5! = 4! x 5
+    // 4! = 3! x 4
+    // 3! = 2! x 3
+    // 2! = 2! x 1
+    // 1! = 1
+    // quy ước 1! = 0! = 1
+    // chốt hạ: n! = n * (n-1)!
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument. N must "
                     + "be between 0..20");
         }
-
+        
         if (n == 0 || n == 1) {
             return 1;
         }
         
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        
-        return product;
+        return n * getFactorial(n - 1);
     }
 }
